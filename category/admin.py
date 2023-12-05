ffb319993bdf424c2bd66a4519795e00e27dd195
kseniyaav <kseniyaav10@gmail.com>
2023-12-05 18:13:11 +0300

    - admin added

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from category.models import Category
+
+admin.register(Category)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name',)
--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import Product
+
+admin.register(Product)
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'piece_price', 'category',)
+    list_filter = ('category',)
